c2bdb42c23cf76aaae0021efd00a8034d5ba742c
gh0stonio <antoine.leveque.pro@gmail.com>
2016-05-21 23:29:24 +0200

    Add es6 support

---
+++ main.js
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow = null;
+
+if (process.env.NODE_ENV === 'development') {
+  require('electron-debug')({showDevTools: true});
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    show: false,
+    width: 1024,
+    height: 728
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/app/app.html`);
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.show();
+    mainWindow.focus();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
